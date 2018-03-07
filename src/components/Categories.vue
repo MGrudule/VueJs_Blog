1985d31266e57f26d2d6db7f07bed72513a4c699
Maija <hello@maijagrudule.com>
2018-03-07 15:00:08 +0100

    [w8] Categories component

--- src/components/Categories.vue
+++ src/components/Categories.vue
@@ -1,54 +1,44 @@
 <template>
   <div class="admin-categories">
-    <router-link to="/admin">Back to Articles</router-link>
+    <router-link to="/admin">Back </router-link>
 
     <form class="edit" @submit.prevent="postCategory(category)">
-<label class="label" for="name">Name</label>
-<div class="input">
-                          <input class="input" required v-model="category.name" type="text" placeholder="Category name">
-                        </div>
-<div class="row">
-                        <div class="col-xs-12 col-md-6">
-                                                    <label class="label" for="message">Description</label>
-
-
-
-        <textarea class="textarea" required v-model="category.description" type="text" placeholder="Category description"> </textarea>
-
+      <label class="label" for="name">Name</label>
+      <div class="input">
+        <input class="input" required v-model="category.name" type="text" placeholder="Category name">
       </div>
-</div>
+      <div class="row">
+        <div class="col-xs-12 col-md-6">
+          <label class="label" for="message">Description</label>
+
+
+
+          <textarea class="textarea" required v-model="category.description" type="text" placeholder="Category description"> </textarea>
+
+        </div>
+      </div>
       <button type="submit">Submit</button>
     </form>
-<hr>
+
+
+    <hr>
 
     <div  class="">
 
       <transition-group name="list" tag="p">
 
-        <div v-for="(category, index) in categories" :key="category.id" class="list-item">
+        <div v-for="(category, index) in categories" :key="category.id" class="list-item" >
           <div class="col-l-6 ">
             <div>
-              <h3>{{ category.name }} {{ category.id }}</h3>
+              <h3>{{ category.name }} </h3>
               <div v-html="category.description"></div>
 
-              <button  class="" v-on:click="deleteCategory(category.id, index)"> Delete Category </button>
-              <router-link tag="button" v-bind:to="'/admin/categories/edit/'+ category.id">Edit</router-link>
-
             </div>
+            <EditCategory :data="categories, category"  v-bind:index="index" v-on:deleted="deleteCategory(index)"></EditCategory>
           </div>
-          <form class="edit" @submit.prevent="updateCategory(category, index)">
 
-            <p>
-              <input type="text" v-model="category.name">
 
-            </p>
-            <p>
-              <textarea id="categoryDescription" type="text" v-model="category.description" rows="20"> </textarea>
 
-            </p>
-
-            <button type="submit">Update</button>
-          </form>
         </div>
 
       </transition-group>
@@ -60,19 +50,27 @@
 <script>
 
 import axios from 'axios'
+import EditCategory from "./EditCategory.vue"
+
 
 export default {
 
   name: 'Categories',
+  components: {
+     'EditCategory': EditCategory ,
+
+   },
 
   data () {
     return {
+
       category: [],
       categories: [],
       loading: false,
       name: '',
       description: '',
       user_id: localStorage.getItem('user_id'),
+
     }
   },
 
@@ -93,23 +91,8 @@ export default {
 
   methods: {
 
-    deleteCategory: function (value, index) {
-      this.loading = true;
-      axios.delete("http://peaceful-dusk-59248.herokuapp.com/api/categories/" + value,
-      {
-        headers: { Authorization: "Bearer " + localStorage.getItem('api_token') }
-      })
-
-      .then((response)  =>  {
-        this.loading = false;
-
-        this.$delete(this.categories, index)
 
 
-      }, (error)  =>  {
-        this.loading = false;
-      })
-    },
 
     postCategory: function (category) {
 this.loading = true;
@@ -129,27 +112,29 @@ this.loading = true;
             this.loading = false;
           })
         },
-        updateCategory: function (category, index) {
+        deleteCategory: function (index) {
 
-        var params = new URLSearchParams();
-        params.append('name', category.name);
-        params.append('description', category.description);
+          var category = this.categories[index];
 
           this.loading = true;
-          axios.put("http://peaceful-dusk-59248.herokuapp.com/api/categories/" + this.$route.params.id,
-                params,
-            {
+          axios.delete("http://peaceful-dusk-59248.herokuapp.com/api/categories/" + category.id,
+          {
             headers: { Authorization: "Bearer " + localStorage.getItem('api_token') }
-            })
+          })
 
           .then((response)  =>  {
-           this.$update(this.categories, index);
+            this.loading = false;
+
+            this.$delete(this.categories, index)
+
 
           }, (error)  =>  {
             this.loading = false;
           })
-        }
-      }
+        },
+      },
+
+
     }
 </script>
 
---
+++ src/components/EditCategory.vue
@@ -0,0 +1,72 @@
+<template id="EditCategory">
+  <div>
+
+    <form  v-show="isActive" class="edit" @submit.prevent="updateCategory(category, index)">
+
+      <p>
+        <input type="text" v-model="category.name">
+
+      </p>
+      <p>
+        <textarea id="categoryDescription" type="text" v-model="category.description" > </textarea>
+
+      </p>
+
+      <button type="submit">Update</button>
+    </form>
+    <button @click="toggle()">{{buttonText}}</button> <button  class="" v-on:click="deleteCategory(index)"> Delete Category </button>
+</div>
+</template>
+
+<script>
+import axios from 'axios'
+export default {
+
+  name: 'EditCategory',
+   props: ['categories','category', 'index'],
+
+   data() {
+    return {
+      isActive: false,
+      buttonText: "Edit",
+    }},
+   methods: {
+     toggle() {
+
+
+       console.log(this.index)
+
+        console.log(this.$parent.categories)
+       this.isActive = !this.isActive;
+       this.buttonText = this.isActive ? 'Close' : 'Edit'
+     },
+     deleteCategory: function (value, index) {
+       console.log(index);
+this.$emit("deleted", index);
+     },
+
+     updateCategory: function (category, index) {
+
+       console.log()
+
+     var params = new URLSearchParams();
+     params.append('name', category.name);
+     params.append('description', category.description);
+
+       this.loading = true;
+       axios.put("http://peaceful-dusk-59248.herokuapp.com/api/categories/" + category.id,
+             params,
+         {
+         headers: { Authorization: "Bearer " + localStorage.getItem('api_token') }
+         })
+
+       .then((response)  =>  {
+       this.loading = false;
+
+       }, (error)  =>  {
+         this.loading = false;
+       })
+     }
+   }
+}
+</script>
